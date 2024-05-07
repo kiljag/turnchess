a8a8b6c52bd1868db9b072a72837976ef5ec7319
Jagadeesh <kjaga12dev@gmail.com>
2024-05-07 23:06:20 +0530

    change port to 9090

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 
 import chessServer from './chess/wsocket';
 
-const CHESS_PORT = 9080;
+const CHESS_PORT = 9090;
 
 chessServer.listen(CHESS_PORT, () => {
     console.log(`server listening on port ${CHESS_PORT}`);
